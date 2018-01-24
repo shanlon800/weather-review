10999e1049e2f6e8e380409d85e3c6ce21487ff9
Sergiu Frunza <sergiufrunza@Sergius-MacBook-Pro.local>
2018-01-24 15:45:24 -0500

    Change `application.js` to include code from master branch

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,15 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+console.log('You\'re doing great!')
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import App from '../App'
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
