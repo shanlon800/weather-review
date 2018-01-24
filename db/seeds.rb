8bdd5a0ece1dbe299e39851843d440dec7871e12
josh hembree <eerbmeh@gmail.com>
2018-01-24 11:00:32 -0500

    updated gems to include faker. created seed file. removed [ from badges.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CITIES = [
+  {
+    city: "Boston",
+    state: "MA",
+    user_id: User.first.id
+  },
+  {
+    city: "Seattle",
+    state: "WA",
+    user_id: User.first.id
+  },
+  {
+    city: "Austin",
+    state: "TX",
+    user_id: User.first.id
+  },
+  {
+    city: "Louisville",
+    state: "KY",
+    user_id: User.first.id
+  },
+  {
+    city: "Boston",
+    state: "MA",
+    user_id: User.first.id
+  },
+  {
+    city: "New York",
+    state: "NY",
+    user_id: User.first.id
+  },
+  {
+    city: "Detroit",
+    state: "MI",
+    user_id: User.first.id
+  }
+]
+
+User.create(email: "email@example.com", password: "password1234")
+
+
+
+CITIES.each do |city|
+  City.create(city_name: city[:city], state: city[:state], user_id: city[:user_id])
+end
+
+35.times {
+  city = City.find(id = rand(CITIES.length)+1)
+  body = Faker::Lorem.paragraph
+  Review.create(city_id: city.id, user_id: User.first.id, body: "#{body}", comfort_index: rand(5)+1, weather_variance: rand(5)+1)
+}
