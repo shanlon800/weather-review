c62c356adace87f4585747a47bda8a85b0a43b48
Sean Hanlon <shanlon800@gmail.com>
2018-01-24 15:56:34 -0500

    RemovedReview: each indiv review tile.  Created universal path in routes

--- app/javascript/components/ReviewShowTile.js
+++ app/javascript/components/ReviewShowTile.js
@@ -3,7 +3,6 @@ import React from 'react';
 const ReviewShowTile = props => {
   return(
     <div>
-      <h3>Reviews:</h3>
       <h5>{props.body}</h5>
       <div>
         <h7>Comfort Index: {props.comfort_index}</h7>
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   devise_for :users
 
-  get '/' => 'static_pages#index'
-  get '/cities' => 'static_pages#index'
-  get '/cities/:id' => 'static_pages#index'
+  get '*/path' => 'static_pages#index'
 
   namespace :api do
     namespace :v1 do
