f7b6c350f0800fe3cedd3c22ed6e97937cf9aa2b
Sean Hanlon <shanlon800@gmail.com>
2018-01-25 14:35:46 -0500

    added to reviews api controller.

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::ReviewsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
   def index
     city_id = params[:city_id]
     city_reviews = Review.where(city_id: city_id)
     render json: city_reviews
+  end
+
+  def create
+    data = JSON.parse(request.body.read)
+    binding.pry
   end
 end
