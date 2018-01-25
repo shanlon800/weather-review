3f0ab39bf44eb363661dc2af32c8d89396540663
Sean Hanlon <shanlon800@gmail.com>
2018-01-25 13:13:58 -0500

    added test for when user does not log in.

--- spec/api/v1/users_controller_spec.rb
+++ spec/api/v1/users_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     let!(:user) {FactoryBot.create(:user, id: 99)}
     let!(:city_one) {City.create(city_name: "Boston", state: "MA", user_id: user.id)}
     let!(:review_one) { Review.create!(city_id: city_one.id, user_id: user.id, body: "Boston is alright. It gets cold.", comfort_index: 3, weather_variance: 4) }
-    it 'should return the current user and the list of their reviews' do
+    it 'should return the signed in current user and the list of their reviews' do
 
       sign_in(user, :scope => :user)
       get :index
@@ -22,6 +22,27 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       expect(returned_json["current_user"]["email"]).to eq user.email
       expect(returned_json["reviews"][0]["body"]).to eq review_one.body
       expect(returned_json["cities"][0]["city_name"]).to eq city_one.city_name
+
+    end
+  end
+
+  describe "GET#index" do
+    let!(:user) {FactoryBot.create(:user, id: 99)}
+    let!(:city_one) {City.create(city_name: "Boston", state: "MA", user_id: user.id)}
+    let!(:review_one) { Review.create!(city_id: city_one.id, user_id: user.id, body: "Boston is alright. It gets cold.", comfort_index: 3, weather_variance: 4) }
+    it 'if no user is signed out, it should return null and an error message' do
+
+
+      get :index
+
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+
+      expect(response.content_type).to eq('application/json')
+      expect(returned_json.length).to eq 2
+
+      expect(returned_json["current_user"]).to eq nil
+      expect(returned_json["message"]).to eq "Please sign in."
 
     end
   end
