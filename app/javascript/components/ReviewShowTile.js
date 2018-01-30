cd584b6f94e7c91154b903cd95626ef28528f84a
Sean Hanlon <shanlon800@gmail.com>
2018-01-30 13:59:38 -0500

    added controller to delete review. wrote tests for create & destroy

--- app/javascript/components/ReviewShowTile.js
+++ app/javascript/components/ReviewShowTile.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 const ReviewShowTile = props => {
+
+  if (props.creator === props.currentUser) {
+    return(
+      <div>
+        <h5>{props.body}</h5>
+        <div>
+          <h7>Comfort Index: {props.comfort_index}</h7>
+        </div>
+        <p>Weather Variance: {props.weather_variance}</p>
+        <button onClick={props.handleDelete}>Delete</button>
+      </div>
+    )
+
+  }
   return(
     <div>
       <h5>{props.body}</h5>
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cities, only: [:index, :show, :create] do
-        resources :reviews, only: [:index]
+        resources :reviews, only: [:index, :destroy]
       end
 
-      resources :reviews, only: [:create]
+      resources :reviews, only: [:create, :destroy]
       resources :users, only: [:index]
     end
   end
