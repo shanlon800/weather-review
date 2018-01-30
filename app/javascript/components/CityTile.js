04fa3ad63ffe414f4e215d32119140c46dc126d1
Sergiu Frunza <sergiufrunza@Sergius-MacBook-Pro.local>
2018-01-30 15:47:52 -0500

    Added banner to the test file for CityTile

--- app/javascript/components/CityTile.js
+++ app/javascript/components/CityTile.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const CityTile = props => {
-
   if (props.banner.url != null ){
+
     return(
       <div class="container">
         <Link to={`/cities/${props.id}`}>
           <h3 class="namestate">{props.city_name} {props.state}</h3>
         </Link>
-        <p class="description">{props.description}</p>
+        <li class="description">{props.description}</li>
 
         <img class="banner" src={props.banner.url} />
       </div>
@@ -20,7 +20,7 @@ const CityTile = props => {
         <Link to={`/cities/${props.id}`}>
           <h3 class="namestate">{props.city_name} {props.state}</h3>
         </Link>
-        <p class="description">{props.description}</p>
+        <li class="description">{props.description}</li>
       </div>
     )
   }
