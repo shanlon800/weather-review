f6d17f68f94c0b3119ca169cb45bfb4308f01deb
Sean Hanlon <shanlon800@gmail.com>
2018-01-31 12:15:33 -0500

    added schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126161514) do
+ActiveRecord::Schema.define(version: 20180131154614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.string "city_name", null: false
     t.string "state", null: false
     t.text "description"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20180126161514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
