0a55e6df0322da9d852882b6f6e80c066047abb6
Sean Hanlon <shanlon800@gmail.com>
2018-02-01 16:55:15 -0500

    added conditional statement to the application.js page for rails pages.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,10 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import App from '../App'
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('app')
-);
+let element = document.getElementById('app')
+if(element){
+  ReactDOM.render(
+    <App/>,
+    element
+  );
+}
